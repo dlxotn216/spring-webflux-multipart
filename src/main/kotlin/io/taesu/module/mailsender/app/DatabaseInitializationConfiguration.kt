528f8c5880fd795557ef48805a27fd54ecc562f3
Lee Tae Su <taesu@crscube.co.kr>
2021-04-02 16:20:52 +0900

    Postgresql로 데이터베이스 환경 마이그레이션

---
+++ src/main/kotlin/io/taesu/module/mailsender/app/DatabaseInitializationConfiguration.kt
@@ -0,0 +1,27 @@
+package io.taesu.module.mailsender.app
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.util.StreamUtils
+import java.nio.charset.Charset
+
+/**
+ * Created by itaesu on 2021/04/02.
+ *
+ * @author Lee Tae Su
+ * @version TBD
+ * @since TBD
+ */
+@Configuration(proxyBeanMethods = false)
+class DatabaseInitializationConfiguration {
+    @Autowired
+    fun initializeDatabase(r2dbcEntityTemplate: R2dbcEntityTemplate) {
+        val schema = StreamUtils.copyToString(
+            ClassPathResource("schema.sql").inputStream,
+            Charset.defaultCharset()
+        )
+        r2dbcEntityTemplate.databaseClient.sql(schema).fetch().rowsUpdated().block()
+    }
+}
